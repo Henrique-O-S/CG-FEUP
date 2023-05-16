e5936699357e29eff7ed69c7faec03eecb54c9a8
Tiago Branquinho <up202005567@edu.fe.up.pt>
2023-05-16 12:17:37 +0100

    Corrected nest bug

--- project/MyNest.js
+++ project/MyNest.js
@@ -15,15 +15,14 @@ export class MyNest extends CGFobject {
         this.position = position;
 
         this.initBuffers();
-        this.Materials();
-        this.Textures();
+        this.initMaterials();
     }
 
     initBuffers() {
         this.cone = new MyCone(this.scene, 6, 6);
     }
 
-    Materials() {
+    initMaterials() {
         //Trunk material (brown)
         this.nestMaterial = new CGFappearance(this.scene);
 
@@ -31,12 +30,7 @@ export class MyNest extends CGFobject {
         this.nestMaterial.setDiffuse(0.7, 0.5, 0.3, 1.0);
         this.nestMaterial.setSpecular(0,0,0, 1.0);
         this.nestMaterial.setShininess(10.0);
-    }
-
-    Textures() {
-        //nest texture
         this.nestTexture = new CGFtexture(this.scene, 'images/nest.jpg');
-
         this.nestMaterial.setTexture(this.nestTexture);
     }
 
@@ -44,18 +38,15 @@ export class MyNest extends CGFobject {
     
         this.scene.pushMatrix();
 
-
         this.scene.translate(this.position.x, this.position.y, this.position.z);
 
         this.nestMaterial.apply();
 
-        this.scene.pushMatrix();
         this.scene.rotate(Math.PI / 180 * 180, 1, 0, 0);
         this.scene.scale(2,1,2);
         this.cone.display();
         this.scene.rotate(Math.PI / 180 * 30, 0, 1, 0);
         this.cone.display();
-        this.scene.popMatrix();
 
         this.scene.popMatrix();
 
